52ba47900b55aeddbfc0f34d48dc7728beb29647
Raina Peneton <rainadawn@msn.com>
2019-09-27 04:26:44 +0000

    36 New lesson form modal

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -18,6 +18,7 @@ class Instructor::CoursesController < ApplicationController
 	def show
 		@current_course = Course.find(params[:id])	
 		@section = Section.new
+		@lesson = Lesson.new
 	end
 
 	private
