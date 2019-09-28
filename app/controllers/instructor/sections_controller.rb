0054794ab01d64826380e08416e7023a33adc2b2
Raina Peneton <rainadawn@msn.com>
2019-09-28 02:55:51 +0000

    final touches

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -18,6 +18,11 @@ class Instructor::SectionsController < ApplicationController
 
 	private
 
+	def require_authorized_for_current_section
+		if current_section.course.user != current_user
+			render plain: "Unauthorized", status: :unauthorized
+		end
+	end
 	def require_authorized_for_current_course
 		if current_course.user != current_user
 			render plain: "Unauthorized", status: :unauthorized
@@ -37,4 +42,3 @@ class Instructor::SectionsController < ApplicationController
 		params.require(:section).permit(:title, :row_order_position)
 	end
 end
- 
